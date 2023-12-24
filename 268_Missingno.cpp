e8a59d2b4a8406779096f5a92cd4020d5ee7e963
Adarsh <protoyoru07@gmail.com>
2023-12-24 19:52:54 +0530

    Missing No. !!

---
+++ 268_Missingno.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+	int missingNumber(vector<int>&nums){
+		int sumall=0;
+		int sumnums=0;
+		for(int i=0;i<=nums.size();i++){
+			sumall+=i;
+		}
+		for(int i=0;i<nums.size();i++){
+			sumnums+=nums[i];
+		}
+	return sumall-sumnums;
+	}
+};
+
+int main(){
+	Solution s1;
+	vector<int>nums={0,2,3};
+	cout<<s1.missingNumber(nums);
+return 0;
+}

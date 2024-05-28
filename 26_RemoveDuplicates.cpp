8e95d311be22b8f7102e8aa356a041147f73d23a
Adarsh <protoyoru07@gmail.com>
2024-05-29 00:06:54 +0530

    Optimized Solution !!

--- 26_RemoveDuplicates.cpp
+++ 26_RemoveDuplicates.cpp
@@ -3,6 +3,23 @@ using namespace std;
 
 class Solution {
 public:
+	void removeDuplicate(vector<int>&num){
+		int i=0;
+		int j=1;
+		while(j<num.size()){
+			if(num[i]==num[j]){
+				j++;
+			}else{
+				num[i+1]=num[j];
+				i++;
+				j++;
+			}
+		}
+		cout<<"The Array after removeDuplicate is :: ";
+		for(int h=0;h<=i;h++){
+			cout<<num[h]<<" ";
+		}
+	}
 	int removeDuplicates(vector<int>&nums){
 		set<int>s;
 		for(int i=0;i<nums.size();i++){
@@ -21,9 +38,10 @@ public:
 int main(){
 	Solution s1;
 	vector<int>nums={11,11,14,23,23,23,56,56,56,56};
-	cout<<s1.removeDuplicates(nums)<<endl;
-	for(int i=0;i<nums.size();i++){
-		cout<<nums[i]<<" ";
-	}	
+	/* cout<<s1.removeDuplicates(nums)<<endl; */
+	/* for(int i=0;i<nums.size();i++){ */
+	/* 	cout<<nums[i]<<" "; */
+	/* } */	
+	s1.removeDuplicate(nums);
 return 0;
 }

2a42530e495e448d4b1b4598e0cd939177fb3760
Adarsh <protoyoru07@gmail.com>
2024-05-30 03:39:06 +0530

    Using Kadane's Algorithm !!

--- 53_MaximumSubarray.cpp
+++ 53_MaximumSubarray.cpp
@@ -1,20 +1,43 @@
 #include <algorithm>
 #include<bits/stdc++.h>
+#include <climits>
 using namespace std;
 
 class Solution {
 public:
 	int maxSubArray(vector<int>&nums){
-		int maxsum=nums[0];
-		int countsum=0;
-		for(int i=0;i<nums.size();i++){
-			if(countsum<0){
-				countsum=0;
+		int maxel=INT_MIN;
+		int sum=0;
+		int i=0;
+		int start=0;
+		int end;
+		while(i<nums.size()){
+			int temp=sum+nums[i];
+			if(temp<0){
+				i++;
+				sum=0;
+				start=i;
 			}
-			countsum=countsum+nums[i];
-			maxsum=max(maxsum,countsum);
+			else{
+				sum=temp;
+				if(sum>maxel)end=i;
+				maxel=max(maxel,sum);
+				i++;
+			}
 		}
-		return maxsum;
+		if(maxel==INT_MIN){
+			int tem=maxel;
+			for(auto it:nums){
+				if(nums[i]>tem)tem=nums[i];
+			}
+			return tem;
+		}
+		cout<<"The Max SubArray Is :: ";
+		for(int i=start;i<=end;i++){
+			cout<<nums[i]<<" ";
+		}
+		cout<<endl;
+		return maxel;
 	}	
 };
 

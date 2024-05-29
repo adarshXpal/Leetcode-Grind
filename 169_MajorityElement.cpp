e7554456fe8a2c6a283d4baad426de6e77244bed
Adarsh <protoyoru07@gmail.com>
2024-05-30 01:40:39 +0530

    Moore's Voting Algorithm !!

--- 169_MajorityElement.cpp
+++ 169_MajorityElement.cpp
@@ -4,14 +4,24 @@ using namespace std;
 class Solution {
 public:
 	int majorityElement(vector<int>&nums){
-		map<int,int>mp;
+		int cnt=0;
+		int el;
 		for(int i=0;i<nums.size();i++){
-			mp[nums[i]]++;
-		}
-		for(auto i:mp){
-			if(i.second>nums.size()/2){
-				return i.first;
+			if(cnt==0){
+				cnt=1;
+				el=nums[i];
 			}
+			else if(nums[i]==el)cnt++;
+			else {
+			cnt--;
+			}
+		}
+		int temp=0;
+		for(auto it:nums){
+			if(it==el)temp++;
+		}
+		if(temp>nums.size()/2){
+			return el;
 		}
 		return -1;
 	}

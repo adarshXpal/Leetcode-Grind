8fad699d285346602ed35a2f5d91b46961e73d5f
Adarsh <protoyoru07@gmail.com>
2024-07-29 21:15:55 +0530

    Binary Search approach !!

--- 34_firstandlastposition.cpp
+++ 34_firstandlastposition.cpp
@@ -3,47 +3,41 @@ using namespace std;
 
 class Solution {
 public:
-    int lowerbound(vector<int>&nums,int target){
-        int low=0;
-        int high=nums.size()-1;
-        int ans=nums.size();
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]>=target){
-                ans=mid;
-                high=mid-1;
-            }else{
-                low=mid+1;
-            }
-        }
-        return ans;
-    }
-    int upperbound(vector<int>&nums,int target){
-        int low=0;
-        int high=nums.size()-1;
-        int ans=nums.size();
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]>target){
-                ans=mid;
-                high=mid-1;
-            }else{
-                low=mid+1;
-            }
-        }
-        return ans-1;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        if(nums[lowerbound(nums,target)]!=target || lowerbound(nums,target)==nums.size()){
-            return {-1,-1};
-        }
-        return {lowerbound(nums,target),upperbound(nums,target)};
+		int first=-1;
+		int last=-1;
+		int low=0;
+		int high=nums.size()-1;
+		while(low<=high){
+			int mid=(low+high)/2;
+			if(nums[mid]>=target){
+				if(nums[mid]==target)first=mid;
+				high=mid-1;
+			}
+			else {
+				low=mid+1;
+			}
+		}
+		if(first==-1)return {-1,-1};
+		low=0;
+		high=nums.size()-1;
+		while(low<=high){
+			int mid=(low+high)/2;
+			if(nums[mid]<=target){
+				if(nums[mid]==target)last=mid;
+				low=mid+1;
+			}
+			else{
+				high=mid-1;
+			}
+		}
+		return {first,last};
     }
 };
 int main(){
 	Solution s1;
-	vector<int>nums={1};
-	int target = 1;
+	vector<int>nums={5,7,7,8,8,10};
+	int target=8;
 	vector<int>ans = s1.searchRange(nums,target);
 	for(auto it:ans){
 		cout<<it<<" ";

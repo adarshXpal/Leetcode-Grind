47ec1e1c8226f56b05fcaa5e17bc8d47dcb22075
Adarsh <protoyoru07@gmail.com>
2024-09-04 17:50:13 +0530

    Recent changes

--- EuclideanAlgorithm.cpp
+++ EuclideanAlgorithm.cpp
@@ -1,18 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-	int n1,n2;
-	cin>>n1>>n2;
-	while(n1!=0&&n2!=0){
-		if(n1>n2){
-			n1%=n2;
-		}else{
-			n2%=n1;
-		}
-	}
-	if(n1==0) cout<<"GCD :: "<<n2;
-	else cout<<"GCD :: "<<n1;
-	return 0;
+int main() {
+  int n1, n2;
+  cin >> n1 >> n2;
+  while (n1 != 0 && n2 != 0) {
+    if (n1 > n2) {
+      n1 %= n2;
+    } else {
+      n2 %= n1;
+    }
+  }
+  if (n1 == 0)
+    cout << "GCD :: " << n2;
+  else
+    cout << "GCD :: " << n1;
+  return 0;
 }
-

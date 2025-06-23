ac44a405f56500e2a574598a67a3f965f7078323
Adarsh <protoyoru07@gmail.com>
2025-06-23 14:49:45 +0530

    Simalar to VerticalTraversalTree !!

---
+++ bottomViewOfBinaryTree_GFG.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+  vector<int> bottomView(Node *root) {
+    vector<int> ans;
+    if (root == NULL) {
+      return ans;
+    }
+    map<int, vector<int>> mp;
+    queue<pair<Node *, int>> q;
+    q.push(make_pair(root, 0));
+    while (!q.empty()) {
+      pair<Node *, int> temp = q.front();
+      q.pop();
+      mp[temp.second].push_back(temp.first->data);
+      if (temp.first->left) {
+        q.push(make_pair(temp.first->left, temp.second - 1));
+      }
+      if (temp.first->right) {
+        q.push(make_pair(temp.first->right, temp.second + 1));
+      }
+    }
+    for (auto i : mp) {
+      ans.push_back(i.second[i.second.size() - 1]);
+    }
+    return ans;
+  }
+};

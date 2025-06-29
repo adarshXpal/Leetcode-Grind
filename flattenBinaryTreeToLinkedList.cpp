b4d742b10d180f794a0c6da850e217b6a524cd26
Adarsh <protoyoru07@gmail.com>
2025-06-29 15:03:59 +0530

    Good question based on morris traversal !!

---
+++ flattenBinaryTreeToLinkedList.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+  void flatten(Node *root) {
+    Node *curr = root;
+    while (curr) {
+      if (curr->left) {
+        Node *temp = curr->left;
+        while (temp->right) {
+          temp = temp->right;
+        }
+        temp->right = curr->right;
+        curr->right = curr->left;
+        curr->left = NULL;
+        curr = curr->right;
+      } else {
+        curr = curr->right;
+      }
+    }
+  }
+};

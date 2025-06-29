22a538000a0d4ccd735351cd3f951e1521ac7728
Adarsh <protoyoru07@gmail.com>
2025-06-29 15:06:04 +0530

    just minor update !!

--- flattenBinaryTreeToLinkedList.cpp
+++ flattenBinaryTreeToLinkedList.cpp
@@ -1,20 +1,19 @@
 class Solution {
-public:
-  void flatten(Node *root) {
-    Node *curr = root;
-    while (curr) {
-      if (curr->left) {
-        Node *temp = curr->left;
-        while (temp->right) {
-          temp = temp->right;
+  public:
+    void flatten(Node *root) {
+        Node*curr=root;
+        while(curr){
+            if(curr->left){
+                Node*temp=curr->left;
+                while(temp->right){
+                    temp=temp->right;
+                }
+                temp->right=curr->right;
+                curr->right=curr->left;
+                curr->left=NULL;
+            }
+            curr=curr->right;
         }
-        temp->right = curr->right;
-        curr->right = curr->left;
-        curr->left = NULL;
-        curr = curr->right;
-      } else {
-        curr = curr->right;
-      }
+        
     }
-  }
 };
